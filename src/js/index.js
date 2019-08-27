1ed31732fff0d3bbea693aac65be7d9163601229
vkoll29 <awino_g@outlook.com>
2019-08-27 16:12:36 +0300

    initial commit. want to try out my own stuff from this point

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+import Search from './models/Search'
+
+/** Global State of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+
+
+
+
+
+
+const search = new Search('pizza')
+search.getResults()
---
+++ src/js/models/Search.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+        const key = "2a32365395e1123f8fa8b0fbca2cf1f4";
+        try{
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);            
+            this.recipes = res.data.recipes;
+            console.log(this.recipes);
+            
+        }catch(error){
+            console.log(error);
+            
+        }
+    }
+}
