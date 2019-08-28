27eb5ce9c6937b17f86a5d628680451f33dbf9ce
vkoll29 <awino_g@outlook.com>
2019-08-28 15:21:39 +0300

    implemented search view and model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
-import Search from './models/Search'
+import Search from './models/Search';
+import * as searchView from './views/SearchvView';
+import {elements} from './views/base'
 
 /** Global State of the app
  * - Search object
@@ -7,12 +9,30 @@ import Search from './models/Search'
  * - Liked recipes
  */
 const state = {};
+const controlSearch = async () => {
+    //1. Get query from view
+    const query = searchView.getInput()
+    console.log(query);
+    
 
+    //2. New Search object and add to state
+    state.search = new Search(query);
 
+    //3. Prepare UI for results
+    searchView.clearInput()
+    searchView.clearResults()
 
+    //4. Search for recipes
+    await state.search.getResults();
 
+    //5. Render results on UI
+    console.log(state.search.recipes);
 
+    searchView.renderRecipes(state.search.recipes) 
+       
+}
 
-
-const search = new Search('pizza')
-search.getResults()
+elements.searchForm.submit(e => {
+    e.preventDefault();
+    controlSearch();
+});
---
+++ src/js/views/base.js
@@ -0,0 +1,8 @@
+import jquery from 'jquery';
+const $ = jquery;
+
+export const elements = {
+    searchForm: $('.search'),
+    searchInput: $('.search__field'),
+    searchResList: $('.results__list')
+}
